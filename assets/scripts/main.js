f85c43cb1f840ee587de2e2cb5f7b52db15e3960
Osmar Baia <osmar.neto23@gmail.com>
2024-06-14 14:57:16 -0300

    Navbar rework, clean up

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,68 +1,15 @@
-/* Header */
-const menuButton = document.querySelector('#menu-btn');
-const menuButtonOpen = document.querySelector('#menu-btn_open');
-const menuButtonClose = document.querySelector('#menu-btn_close');
-const sideMenu = document.querySelector('#sideMenu');
-const headerMenu = document.querySelector('header');
-
-menuButton.addEventListener('click', toggleMenu)
-
-function toggleMenu() {
-    menuButtonOpen.classList.toggle('hidden');
-    menuButtonClose.classList.toggle('hidden');
-    toggleSideMenu();
-}
-
-
 /*
-* SideMenu
+* HEADER
 * */
-function toggleSideMenu() {
-    const animationDuration = 500;
-    if (sideMenu.classList.contains('hidden')) {
-        sideMenu.classList.add('slide-in-right');
-        sideMenu.classList.toggle('hidden');
-        disableScroll();
-        setTimeout(() => {
-            sideMenu.classList.remove('slide-in-right');
-        }, animationDuration);
-    } else {
-        sideMenu.classList.add('slide-out-right');
-        setTimeout(() => {
-            sideMenu.classList.remove('slide-out-right');
-            sideMenu.classList.toggle('hidden');
-            enableScroll();
-        }, animationDuration);
-    }
-}
-function disableScroll() {
-    // Get the current scroll position
-    const scrollY = window.scrollY;
+const menu = document.querySelector('#menu-btn');
+const navbar = document.querySelector('.navbar');
 
-    // Save the current scroll position as a negative top offset
-    document.body.style.position = 'fixed';
-    document.body.style.top = `-${scrollY}px`;
-}
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+})
 
-function enableScroll() {
-    // Retrieve the previous scroll position
-    const scrollY = parseInt(document.body.style.top || '0', 10);
-
-    // Restore the scroll position and reset styles
-    document.body.style.position = '';
-    document.body.style.top = '';
-    window.scrollTo(0, Math.abs(scrollY)); // Scroll to the previous position
-}
-
-
-// trigger is mouse delta
-let prevScrollPos = window.scrollY;
-window.onscroll = function() {
-    let currentScrollPos = window.scrollY;
-    if (prevScrollPos > currentScrollPos) {
-        headerMenu.style.top = "0";
-    } else {
-        headerMenu.style.top = "-150px";
-    }
-    prevScrollPos = currentScrollPos;
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
 }
