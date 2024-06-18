f5539d668bf1971223dfb906f1cfbf4a465db0f5
Osmar Baia <osmar.neto23@gmail.com>
2024-06-18 12:06:58 -0300

    Add hide/show effect to navbar

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,28 @@
 /*
 * HEADER
 * */
-const menu = document.querySelector('#menu-btn');
-const navbar = document.querySelector('.navbar');
+const menuBtn = document.querySelector('#menu-btn');
+const navbarMenu = document.querySelector('.navbar');
+const navbar = document.querySelector("#navbar")
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('fa-times');
+    navbarMenu.classList.toggle('active');
 })
 
 window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
+    menuBtn.classList.remove('fa-times');
+    navbarMenu.classList.remove('active');
+}
+
+// Hide/show navbar on scroll
+let prevScrollPos = window.scrollY;
+window.onscroll = function() {
+    let currentScrollPos = window.scrollY;
+    if (prevScrollPos > currentScrollPos) {
+        navbar.style.top = "0";
+    } else {
+        navbar.style.top = "-100px";
+    }
+    prevScrollPos = currentScrollPos;
 }
